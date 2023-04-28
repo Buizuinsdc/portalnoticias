fb1e891d47ac2b01be1cdbbb94c901c1179426ea
Felipe <felipesdcc@gmail.com>
2023-04-28 12:38:22 -0300

    alterações

--- conexao.php
+++ conexao.php
@@ -1,10 +1,10 @@
 
 
 <?php 
-define('HOST', '127.0.0.1');
-define('USUARIO', 	'root');
-define('SENHA', '987449807');
-define('DB', 'portalnoticias');
+define('HOST', 'us-cdbr-east-06.cleardb.net');
+define('USUARIO', 	'b572a9a8bf42ae');
+define('SENHA', 'c6f63de5');
+define('DB', 'heroku_38c9f5ae51df506');
 
 $conexao = mysqli_connect(HOST,USUARIO, SENHA, DB) or die('conexão não realizada! ');
 
