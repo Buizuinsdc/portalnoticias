18ce92598b93b655e05114fa0c0cfbe254bf7d45
Buizuinsdc <132080777+Buizuinsdc@users.noreply.github.com>
2023-05-18 11:43:16 -0300

    Update lista-noticias.php
    Uploud de imagem

--- lista-noticias.php
+++ lista-noticias.php
@@ -7,12 +7,13 @@ $resultado = mysqli_query($conexao, $sql);
 
 // Exibir as notícias em uma tabela HTML
 echo "<table>";
-echo "<tr><th>Título</th><th>Conteúdo</th><th>Autor</th></tr>";
+echo "<tr><th>Título</th><th>Conteúdo</th><th>Autor</th><th>Imagem</th></tr>";
 while ($noticia = mysqli_fetch_assoc($resultado)) {
   echo "<tr>";
   echo "<td>" . $noticia['titulo'] . "</td>";
   echo "<td>" . $noticia['conteudo'] . "</td>";
   echo "<td>" . $noticia['autor'] . "</td>";
+  echo "<td><img src="  . $noticia['arquivo'] .   "></td>";
   echo "</tr>";
 }
 echo "</table>";
