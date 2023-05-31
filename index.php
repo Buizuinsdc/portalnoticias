68eb4c4d8efe1cb315fe1d58422c4ff540111523
Buizuinsdc <132080777+Buizuinsdc@users.noreply.github.com>
2023-05-30 22:46:15 -0300

    FRONT CAIO index.php

--- index.php
+++ index.php
@@ -17,46 +17,35 @@ include('conexao.php');
 </head>
 
 <body>
-    <section class="hero is-success is-fullheight">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Acesso ao Portal de Notícias</h3>
-                    <h3 class="title has-text-grey"><a href="https://www.linkedin.com/in/matheus-lins-santos-440969208/" target="_blank">Matheus Lins</a></h3>
-                    <?php
-                    if(isset($_SESSION['nao_autenticado'])){
-                        echo '<script>alert("Mensagem de alerta!");</script>';
-                    }
-                    ?>
+ <div class="card">
+    <div class="image-container">
+      <img src="img/undraw_News_re_6uub.png" alt="Imagem de fundo">
+    </div>
+    <div class="content-container">
+      <div class="form-container">
+        <h2>Login</h2>
+        <?php
+                if(isset($_SESSION['nao_autenticado']))
+             {
+                echo '<script>alert("Mensagem de alerta!");</script>';
+              }
+              ?>
                     <div class="notification is-danger">
                       <p>ERRO: Usuário ou senha inválidos.</p>
                     </div>
-                   
-                    <div class="box">
-                        <form action="login.php" method="POST">
-                            <div class="field">
-                                <div class="control">
-                                    <input name="email" name="text" class="input is-large" placeholder="Email" autofocus="">
-                                </div>
-                            </div>
-
-                            <div class="field">
-                                <div class="control">
-                                    <input name="senha" class="input is-large" type="password" placeholder="Senha">
-                                </div>
-                            </div>
-                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Entrar</button>
-                            <br>
-                            
-                        </form>
-                        <form action="cadastro.php">
-                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Cadastrar</button>
-                            <form/>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+        <form action="login.php" method="POST">
+          <input type="text" name="email" placeholder="E-mail" required>
+          <input type="password" name="senha" placeholder="Senha" required>
+        </form>
+        <form action="cadastro.php">
+        <button type="submit">Entrar</button>
+        </form>
+        <form action="cadastro.php">
+         <button type="submit" class="button is-block is-link is-large is-fullwidth">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
