11f2fe1b22e7823eee2d564b197bf24a379b2555
caiofgfg <93356369+caiofgfg@users.noreply.github.com>
2023-06-06 18:49:22 -0300

    Update lista-noticias.php

--- lista-noticias.php
+++ lista-noticias.php
@@ -1,56 +1,57 @@
 <?php
-session_start();
 include('conexao.php');
 ?>
 
 <!DOCTYPE html>
-<html lang="pt">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="css/noticiaslist.css">
-  <title>Portal</title>
+  <title>Página de Notícias</title>
+  <link rel="stylesheet" type="text/css" href="estilos.css">
 </head>
 <body>
   <header>
-    <h1>FICRNEWS</h1>
-    <div class="buttons">
-      <h2>Olá!   <?php echo $_SESSION['email'];?>   </h2>
+    <h1>Notícias</h1>
+    <div class="botoes-topo">
+      <a href="#" class="botao-cadastro">Cadastro</a>
+      <a href="#" class="botao-login">Login</a>
     </div>
   </header>
 
-  <?php
-  // Buscar as notícias no banco de dados
-  $sql = "SELECT * FROM noticias";
-  $resultado = mysqli_query($conexao, $sql);
-  ?>
+  <div class="destaque">
+    <h2>Notícia em Destaque</h2>
+    <div class="card-destaque">
+      <img src="caminho-da-imagem.jpg" alt="Imagem da Notícia">
+      <div class="card-content">
+        <h3>Título da Notícia</h3>
+        <p>Conteúdo da notícia...</p>
+        <p class="data">Publicado em: 01 de janeiro de 2023</p>
+      </div>
+    </div>
+  </div>
 
-  <table>
-    <tr>
-      <th>Título</th>
-      <th>Conteúdo</th>
-      <th>Autor</th>
-      <th>Imagem</th>
-    </tr>
+  <div class="noticias">
+    <h2>Outras Notícias</h2>
     <?php
-    $linha = 0;
-    while ($noticia = mysqli_fetch_assoc($resultado)) {
-      $linha++;
-      $cor = $linha % 2 == 0 ? "#f9f9f9" : "#ebebeb";
-      ?>
-      <tr style="background-color: <?php echo $cor; ?>">
-        <td><?php echo $noticia['titulo']; ?></td>
-        <td><?php echo $noticia['conteudo']; ?></td>
-        <td><?php echo $noticia['autor']; ?></td>
-        <td><img src="<?php echo $noticia['arquivo']; ?>"></td>
-      </tr>
-    <?php } ?>
-  </table>
+    $sql = "SELECT * FROM noticias";
+    $resultado = mysqli_query($conexao, $sql);
 
-  <?php
-  // Fechar a conexão com o banco de dados
-  mysqli_close($conexao);
-  ?>
+    while ($noticia = mysqli_fetch_assoc($resultado)) {
+      echo "<div class='card-noticia'>";
+      echo "<img src='" . $noticia['arquivo'] . "' alt='Imagem da Notícia'>";
+      echo "<div class='card-content'>";
+      echo "<h3>" . $noticia['titulo'] . "</h3>";
+      echo "<p>" . $noticia['conteudo'] . "</p>";
+      echo "<p class='data'>Publicado em: " . $noticia['data'] . "</p>";
+      echo "</div>";
+      echo "</div>";
+    }
+
+    mysqli_close($conexao);
+    ?>
+  </div>
+
+  <footer>
+    &copy; 2023 Notícias. Todos os direitos reservados.
+  </footer>
 </body>
 </html>
