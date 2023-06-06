9b9363e51241de27213c3999a7b27366995b2447
Buizuinsdc <132080777+Buizuinsdc@users.noreply.github.com>
2023-06-06 15:55:35 -0300

    Update lista-noticias.php

--- lista-noticias.php
+++ lista-noticias.php
@@ -10,7 +10,7 @@ include('conexao.php');
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="css/noticiaslist.css"
-  <title>Document</title>
+  <title>Portal</title>
 </head>
 <body>
 <?php
