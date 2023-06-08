cfb08a0bc22b3dfa0a6a9b8ee6cbe889859af0e4
Buizuinsdc <132080777+Buizuinsdc@users.noreply.github.com>
2023-06-07 21:47:20 -0300

    Update lista-noticias.php

--- lista-noticias.php
+++ lista-noticias.php
@@ -6,6 +6,7 @@ include('conexao.php');
 <!DOCTYPE html>
 <html>
 
+    
 <head>
     <title>Página de Notícias</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
