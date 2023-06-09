5308513c413221614e9f154d4f9377d51bf0f496
Buiu <buiuzinsdc@gmail.com>
2023-06-09 19:39:09 -0300

    Alterações

--- listanoticia.php
+++ listanoticia.php
@@ -99,7 +99,14 @@ include('conexao.php');
         <div class="area_right">
             <div class="right_full" style="margin:20px;">
                 <div class="searcharea">
+                    <br>
+                    <h6>Àrea de pesquisa</h6>
+                <div class="titlepes" style="display:flex; align-items:center; justify-content:center; height:80px;">
+                <span class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                </svg></span>
                 <input id="searchbar" name="searchbar" type="search" placeholder="pesquisar" onkeyup="search()">
+                </div>
                 </div>
             </div>
         </div>
